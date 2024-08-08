fd31874121a1924f7eb99ce774decd23d048119d
Edwin HC <edwinrhc@gmail.com>
2024-08-08 17:11:07 -0500

    Sección 02. Getters,métodos y THIS

--- typescript-intro/src/base/03-classes.ts
+++ typescript-intro/src/base/03-classes.ts
@@ -1,7 +1,26 @@
 
 export class Pokemon {
-    constructor(public readonly id: number, public name: string) {
+
+    get imageUrl():string {
+        return `https://pokemon.com/${this.id}.jpg`;
+    }
+
+    constructor(
+        public readonly id: number,
+        public name: string
+      ){}
+
+    scream(){
+        console.log(`${this.name.toUpperCase()}!!!!`);
+    }
+    speak(){
+        console.log(`${this.name}, ${this.name}`);
     }
 }
 
-export const charmander = new Pokemon(1,'Leo');
+export const charmander = new Pokemon(4,'Scuare');
+
+console.log(charmander);
+
+charmander.scream();
+charmander.speak();
