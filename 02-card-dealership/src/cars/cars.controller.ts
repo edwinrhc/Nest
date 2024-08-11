277a8d3a544b2c912304657108d135b0db8bdd25
Edwin HC <edwinrhc@gmail.com>
2024-08-11 12:41:02 -0500

    Inyección de dependencias

--- 02-card-dealership/src/cars/cars.controller.ts
+++ 02-card-dealership/src/cars/cars.controller.ts
@@ -1,19 +1,22 @@
 import {Controller, Get, Param} from '@nestjs/common';
+import {CarsService} from "./cars.service";
 
 @Controller('cars')
 export class CarsController {
 
+ constructor(
+     private readonly  carsService: CarsService) {
+ }
 
-    private cars = ['Toyota','Nissan','Jeep'];
 
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll();
     }
 
     @Get(':id')
     getCarById(@Param('id') id:string){
-        console.log(this.cars[id]);
-        return this.cars[id];
+        // console.log({id: id});
+        return this.carsService.findOneById(Number(id));
     }
 }
---
+++ 02-card-dealership/src/cars/cars.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id:2,
+            brand: 'Honda',
+            model: 'Civic'
+        },
+        {
+            id:3,
+            brand: 'Jeep',
+            model: 'Cherokee'
+        }
+    ];
+
+    findAll(){
+        return this.cars;
+    }
+
+    findOneById(id: number){
+        return this.cars.find(car => car.id === id);
+    }
+
+
+}
