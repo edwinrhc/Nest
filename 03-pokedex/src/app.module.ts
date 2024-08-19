b733156851664567fc21050ee60876664d4a9004
Edwin HC <edwinrhc@gmail.com>
2024-08-19 17:27:35 -0500

    add: Mongo

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import {join } from 'path';
 import {ServeStaticModule} from "@nestjs/serve-static";
 import { PokemonModule } from './pokemon/pokemon.module';
+import {MongooseModule} from "@nestjs/mongoose";
 
 
 
@@ -11,6 +12,8 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
   ],
 
