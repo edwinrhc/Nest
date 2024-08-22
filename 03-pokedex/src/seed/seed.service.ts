5ce758c6e6e955d7da0f1e3d6d770ed89bc9f3e5
Edwin HC <edwinrhc@gmail.com>
2024-08-22 10:36:03 -0500

    Realizar petición http desde Nest, usando Axios

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import axios, {AxiosInstance} from "axios";
+import {PokeResponse} from "./interfaces/poke-response.interface";
 
 
 @Injectable()
 export class SeedService {
 
+  // Una dependencia del servicio
+  private readonly  axios: AxiosInstance = axios;
 
-  executeSeed(){
-    return 'Seed executed';
+
+  async executeSeed(){
+    const {data}  = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    data.results.forEach(({name,url}) => {
+      // obtenemos el número de pokemon
+      const segments = url.split('/');
+      const num = +segments[segments.length -2] // la penúltima posición
+      console.log({name, num})
+
+    })
+
+
+
+    return data.results;
+
   }
 
 }
