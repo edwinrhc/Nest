87ba1c28f7a3fc87bddaab1415b3fd5e5323da3a
Edwin HC <edwinrhc@gmail.com>
2024-09-03 14:14:48 -0500

    configure database

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -22,7 +22,9 @@ import {JoiValidationSchema} from "./config/joi.validation";
             rootPath: join(__dirname, '..', 'public'),
         }),
 
-        MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+        MongooseModule.forRoot(process.env.MONGODB, {
+            dbName: 'pokemonsdb'
+        }),
         PokemonModule,
         CommonModule,
         SeedModule,
