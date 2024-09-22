600abf92d71a3a66dcbf53f76efc4ae1398a34cd
Edwin HC <edwinrhc@gmail.com>
2024-09-21 20:18:41 -0500

    Retornar el secureUrl

--- 05-teslo-shop/src/files/files.controller.ts
+++ 05-teslo-shop/src/files/files.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete,UploadedFile,UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,UploadedFile,UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-
+import {Response} from 'express';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+      private readonly filesService: FilesService,
+      private readonly configService: ConfigService,
+      ) {}
+
+
+
+  @Get('product/:imageName')
+  findProductImage(
+      @Res() res: Response,
+      @Param('imageName') imageName: string,
+  ){
+    const path = this.filesService.getStatisProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -25,9 +40,11 @@ export class FilesController {
     if(!file){
       throw new BadRequestException('Make sure that file is an image');
     }
-    console.log(file);
+    // console.log(file);
+    // const secureUrl = `${file.filename}`;
+      const secureUrl= `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
     return {
-      fileName: file.originalname
+      secureUrl
     };
 
   }
--- 05-teslo-shop/src/files/files.service.ts
+++ 05-teslo-shop/src/files/files.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
+
+    getStatisProductImage(imageName:String){
+        // @ts-ignore
+        const path = join(__dirname,'../../static/products',imageName);
+        if(!existsSync(path)){
+            throw new BadRequestException(`No product found with image ${imageName}`);
+        }
+        return path;
+    }
 
 }
