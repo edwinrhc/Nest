065b10717147c13245499bcf31a4f94797b5d65a
Edwin HC <edwinrhc@gmail.com>
2024-09-24 22:47:16 -0500

    Entidad de Usuarios

---
+++ 05-teslo-shop/src/auth/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import {Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text',{
+        unique: true,
+})
+    email: string;
+
+    @Column('text')
+    password: string;
+
+    @Column('text')
+    fullName: string;
+
+    @Column('bool',{
+        nullable:true,
+    })
+    isActive: boolean;
+
+    @Column('text', {
+        array:true,
+        default: ['user']
+    })
+    roles: string[];
+
+}
