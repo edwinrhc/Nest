377163bcb8ccfcc2611c38e88c50832f9c6b27b4
Edwin HC <edwinrhc@gmail.com>
2024-09-29 12:48:38 -0500

    Private Route - General

--- 05-teslo-shop/src/auth/auth.controller.ts
+++ 05-teslo-shop/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginUserDto } from './dto';
+import { AuthGuard } from '@nestjs/passport';
 
 
 @Controller('auth')
@@ -16,6 +17,15 @@ export class AuthController {
   @Post('login')
   loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto);
+  }
+
+  @Get('private')
+  @UseGuards(AuthGuard())
+  testingPrivateRoute(){
+    return {
+      ok:true,
+      message: 'Hola Mundo Private'
+    }
   }
 
 
