756917c23154990049ded9c118bc0fafb8012e8a
Edwin HC <edwinrhc@gmail.com>
2024-10-06 07:33:40 -0500

    Composición de Decoradores

--- 05-teslo-shop/src/auth/auth.controller.ts
+++ 05-teslo-shop/src/auth/auth.controller.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser,RawHeaders } from './decorators';
+import {Auth, GetUser,RawHeaders } from './decorators';
 import { User } from './entities/user.entity';
 import { IncomingHttpHeaders } from 'http';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
@@ -52,6 +52,17 @@ export class AuthController {
   @RoleProtected(ValidRoles.supeUser,ValidRoles.admin)
   @UseGuards(AuthGuard(), UserRoleGuard)
   privateRoute2( @GetUser() user: User){
+
+    return {
+      ok:true,
+      user
+    }
+  }
+
+
+  @Get('private3')
+  @Auth(ValidRoles.admin)
+  privateRoute3( @GetUser() user: User){
 
     return {
       ok:true,
--- 05-teslo-shop/src/auth/decorators/index.ts
+++ 05-teslo-shop/src/auth/decorators/index.ts
@@ -1,2 +1,8 @@
+
 export { GetUser } from "./get-user.decorator"
 export { RawHeaders } from "./raw-header.decorator"
+
+export { RoleProtected } from "./role-protected.decorator";
+export {Auth} from './auth.decorator';
+
+
