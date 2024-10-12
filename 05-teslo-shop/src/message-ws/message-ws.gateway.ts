d5875167e53c8eb14b3b3a533cb814f476faa38e
Edwin HC <edwinrhc@gmail.com>
2024-10-12 18:36:16 -0500

    Server: Escuchar conexiones y desconexiones

--- 05-teslo-shop/src/message-ws/message-ws.gateway.ts
+++ 05-teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,18 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({cors: true})
-export class MessageWsGateway {
-  constructor(private readonly messageWsService: MessageWsService) {}
+export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+  constructor(
+      private readonly messageWsService: MessageWsService
+  ) {}
+
+  handleConnection(client: Socket) {
+    console.log('Cliente conectado', client.id);
+    }
+
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado',client.id);
+    }
 }
