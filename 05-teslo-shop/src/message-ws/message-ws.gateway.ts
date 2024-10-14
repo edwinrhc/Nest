d10ac3fa597c3d5b2e9d21ee58e2dc4767887b5c
Edwin HC <edwinrhc@gmail.com>
2024-10-13 20:54:22 -0500

    Enlaza socket con Usuario

--- 05-teslo-shop/src/message-ws/message-ws.gateway.ts
+++ 05-teslo-shop/src/message-ws/message-ws.gateway.ts
@@ -1,71 +1,76 @@
-import {OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { MessageWsService } from './message-ws.service';
-import {Server, Socket } from 'socket.io';
-import { NewMessageDto } from './dtos/new-message.dto';
-import { JwtService } from '@nestjs/jwt';
-import { JwtPayload } from '../auth/interfaces';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from '@nestjs/websockets';
+import {MessageWsService} from './message-ws.service';
+import {Server, Socket} from 'socket.io';
+import {NewMessageDto} from './dtos/new-message.dto';
+import {JwtService} from '@nestjs/jwt';
+import {JwtPayload} from '../auth/interfaces';
 
 @WebSocketGateway({cors: true})
-export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-  @WebSocketServer() wss: Server;
+    @WebSocketServer() wss: Server;
 
-  constructor(
-      private readonly messageWsService: MessageWsService,
-      private readonly jwtService: JwtService
-  ) {}
-
-  handleConnection(client: Socket) {
-    // console.log('Cliente conectado', client.id);
-    // console.log(client)
-    const token = client.handshake.headers.authentication as string;
-    let payload: JwtPayload;
-    try{
-    payload = this.jwtService.verify(token);
-    }catch (error){
-      client.disconnect();
-      return;
-    }
-    console.log({payload});
-
-    this.messageWsService.registerClient(client);
-    // console.log({conectados: this.messageWsService.getConnectedClients()});
-
-    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
-
+    constructor(
+        private readonly messageWsService: MessageWsService,
+        private readonly jwtService: JwtService
+    ) {
     }
 
-  handleDisconnect(client: Socket) {
+    async handleConnection(client: Socket) {
+        // console.log('Cliente conectado', client.id);
+        // console.log(client)
+        const token = client.handshake.headers.authentication as string;
+        let payload: JwtPayload;
+        try {
+            payload = this.jwtService.verify(token);
+            await this.messageWsService.registerClient(client, payload.id);
+        } catch (error) {
+            client.disconnect();
+            return;
+        }
+        // console.log({payload});
+        // console.log({conectados: this.messageWsService.getConnectedClients()});
+        this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+
+    }
+
+    handleDisconnect(client: Socket) {
 
 
-    // console.log('Cliente desconectado',client.id);
-    this.messageWsService.removeClient(client.id);
-    // console.log({conectados: this.messageWsService.getConnectedClients()});
-    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+        // console.log('Cliente desconectado',client.id);
+        this.messageWsService.removeClient(client.id);
+        // console.log({conectados: this.messageWsService.getConnectedClients()});
+        this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
     }
 
     @SubscribeMessage('message-from-client')
     //onMessaggeFromClient
     handleMessageFromClient(client: Socket, payload: NewMessageDto) {
 
-      // message-from-server
-      //! Emite únicamente al cliente.
-      // client.emit('message-from-server',{
-      //   fullName: 'Soy yo',
-      //   message: payload.message || 'no-messsage!!'
-      // });
+        // message-from-server
+        //! Emite únicamente al cliente.
+        // client.emit('message-from-server',{
+        //   fullName: 'Soy yo',
+        //   message: payload.message || 'no-messsage!!'
+        // });
 
-      //! Emitir a todos MENOS, al cliente inicial
-      // client.broadcast.emit('message-from-server',{
-      //   fullName: 'Soy yo',
-      //   message: payload.message || 'no-messsage!!'
-      // });
+        //! Emitir a todos MENOS, al cliente inicial
+        // client.broadcast.emit('message-from-server',{
+        //   fullName: 'Soy yo',
+        //   message: payload.message || 'no-messsage!!'
+        // });
 
-      // Emitir todos los clientes
-      this.wss.emit('message-from-server',{
-        fullName: 'Soy yo!!',
-        message: payload.message || 'no-message!!'
-      });
+        // Emitir todos los clientes
+        this.wss.emit('message-from-server', {
+            fullName: this.messageWsService.getUserFullName(client.id),
+            message: payload.message || 'no-message!!'
+        });
 
     }
 }
--- 05-teslo-shop/src/message-ws/message-ws.service.ts
+++ 05-teslo-shop/src/message-ws/message-ws.service.ts
@@ -1,24 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 interface ConnectedClients {
-    [id: string]: Socket
+    [id: string]: {
+        socket: Socket,
+        user: User
+    }
 }
 
 @Injectable()
 export class MessageWsService {
 
-    private connectdClients: ConnectedClients =  {}
+    private connectedClients: ConnectedClients =  {}
 
-    registerClient(client: Socket){
-        this.connectdClients[client.id] = client;
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {
     }
+
+    async registerClient(client: Socket, userId: string) {
+        const user = await this.userRepository.findOneBy({id: userId});
+        // console.log(user);
+        if( !user) throw new Error('User not found');
+        if( !user.isActive) throw new Error('User not active');
+
+        this.connectedClients[client.id] = {
+            socket: client,
+            user: user
+        };
+    }
+
     removeClient(clientId: string){
-        delete this.connectdClients[clientId];
+        delete this.connectedClients[clientId];
     }
 
     getConnectedClients(): string[] {
-        return Object.keys(this.connectdClients);
+        // console.log(this.connectedClients);
+        return Object.keys(this.connectedClients);
+    }
+
+    getUserFullName(socketId: string){
+        return this.connectedClients[socketId].user.fullName;
     }
 
 }
