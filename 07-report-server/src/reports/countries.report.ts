f91db2963ac2cac9d774c7a6bb021287d7e1f2b7
Edwin HC <edwinrhc@gmail.com>
2024-11-14 12:21:43 -0500

    Estilo de table

--- 07-report-server/src/reports/countries.report.ts
+++ 07-report-server/src/reports/countries.report.ts
@@ -1,7 +1,7 @@
-import { TDocumentDefinitions } from "pdfmake/interfaces";
-import { headerSection } from "./sections/header.section";
-import { countries as Country} from "@prisma/client";
-import { footerSection } from "./sections/footer.section";
+import {TDocumentDefinitions} from "pdfmake/interfaces";
+import {headerSection} from "./sections/header.section";
+import {countries as Country} from "@prisma/client";
+import {footerSection} from "./sections/footer.section";
 
 interface ReportOptions {
     title?: string;
@@ -10,38 +10,72 @@ interface ReportOptions {
 }
 
 
+export const getCountryReport = (options: ReportOptions): TDocumentDefinitions => {
 
-export const getCountryReport = (options: ReportOptions):TDocumentDefinitions => {
-
-    const {title,subTitle, countries}  = options;
+    const {title, subTitle, countries} = options;
 
     return {
         pageOrientation: 'landscape',
         header: headerSection({
-            title: title ??'Countries Report',
+            title: title ?? 'Countries Report',
             subTitle: subTitle ?? 'List of countries',
 
         }),
         footer: footerSection,
-        pageMargins:[40,110,40,60],
-        content:[
+        pageMargins: [40, 110, 40, 60],
+        content: [
             {
-                layout:'lightHorizontalLines',
-                table:{
+                layout: 'customLayout01',
+                table: {
                     headerRows: 1,
-                    widths:[50,50,50,'*','auto','*'],
-                    body:[
-                        ['ID','ISO2','ISO3','Name','Continent','Local Name'],
+                    widths: [50, 50, 50, '*', 'auto', '*'],
+                    body: [
+                        ['ID', 'ISO2', 'ISO3', 'Name', 'Continent', 'Local Name'],
                         ...countries.map((country) => [
                             country.id.toString(),
                             country.iso2,
                             country.iso3,
-                            {text: country.name,bold:true},
+                            {text: country.name, bold: true},
                             country.continent,
                             country.local_name
-                        ])
+                        ]),
+                        ['', '', '', '', '', ``],
+                        ['', '', '', '', 'Total',
+                            {
+                                text: `${countries.length} países`,
+                                bold: true,
+                            }]
                         // ['First','Second','Third','The last one'],
-               //         ['Value 1','Value 2','Value 3','Value 4'], [{text:'Bold value ',bold:true,},'Val 2','Val 3','Val 4' ],
+                        //         ['Value 1','Value 2','Value 3','Value 4'], [{text:'Bold value ',bold:true,},'Val 2','Val 3','Val 4' ],
+                    ]
+                }
+            },
+            {
+                text: 'Totales',
+                style: {
+                    fontSize: 18,
+                    bold: true,
+                    margin: [0, 40, 0, 0]
+                }
+            },
+            {
+                layout: 'noBorders',
+                table: {
+                    headerRows: 1,
+                    widths: [50, 50, 70, '*', 'auto', '*'],
+                    body: [
+                        [
+                            {
+                                text: 'Total de países',
+                                colSpan: 2,
+                                bold: true
+                            },
+                            {},
+                            {
+                                text:`${countries.length} países`,
+                                bold: true
+                            }, {}, {}, {}
+                        ]
                     ]
                 }
             }
