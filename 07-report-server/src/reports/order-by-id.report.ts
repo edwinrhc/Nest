b255cffe2937d83b47d89589b19cf4d95b669751
Edwin HC <edwinrhc@gmail.com>
2024-11-21 23:55:30 -0500

    Información completa del recibo

--- 07-report-server/src/reports/order-by-id.report.ts
+++ 07-report-server/src/reports/order-by-id.report.ts
@@ -23,9 +23,51 @@ const styles: StyleDictionary = {
     }
 }
 
+export interface CompleteOrder {
+    order_id:      number;
+    customer_id:   number;
+    order_date:    Date;
+    customers:     Customers;
+    order_details: OrderDetail[];
+}
 
-export const orderByIdReport = (): TDocumentDefinitions => {
+export interface Customers {
+    customer_id:   number;
+    customer_name: string;
+    contact_name:  string;
+    address:       string;
+    city:          string;
+    postal_code:   string;
+    country:       string;
+}
 
+export interface OrderDetail {
+    order_detail_id: number;
+    order_id:        number;
+    product_id:      number;
+    quantity:        number;
+    products:        Products;
+}
+
+export interface Products {
+    product_id:   number;
+    product_name: string;
+    category_id:  number;
+    unit:         string;
+    price:        string;
+}
+interface ReportValues {
+    title?: string;
+    subTitle?: string;
+    data:CompleteOrder;
+
+}
+
+
+export const orderByIdReport = (value: ReportValues): TDocumentDefinitions => {
+
+    const { data} = value;
+    console.log(data);
 
     return {
         styles: styles,
