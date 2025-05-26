1533830a138e3e1c8c220024f0c9231e1a0c5d3e
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-26 01:22:38 -0500

    feat(donu.chart): Se implemento nueva función getDonutChart

---
+++ 07-report-server/src/reports/charts/donut.chart.ts
@@ -0,0 +1,52 @@
+import * as Utils from '../../helpers/chart-utils';
+
+interface DonuEntry{
+  label: string;
+  value: number;
+}
+
+interface DonutOptions{
+  posicion?: 'left' | 'right' | 'top' | 'bottom',
+  entries: DonuEntry[];
+}
+
+export const getDonutChart = async  (options: DonutOptions) :Promise<string> => {
+  const { posicion = 'top' } = options;
+
+    const data = {
+      labels: options.entries.map((e)  => e.label),
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: options.entries.map((e) => e.value),
+          // backgroundColor: Object.values(Utils.CHART_COLORS),
+        }
+      ]
+    };
+
+    const config = {
+      type: 'doughnut',
+      data: data,
+      options: {
+        responsive: true,
+        legend: {
+          position: posicion,
+        },
+/*        title: {
+          text: 'Chart.js Doughnut Chart',
+          display: true,
+        },*/
+        plugins: {
+          datalabels: {
+            color: 'white',
+            font:{
+              weight: 'bold',
+              size: 14,
+            }
+          },
+        }
+      },
+    };
+
+    return Utils.chartJSToImage(config)
+  }
--- 07-report-server/src/reports/statistics.report.ts
+++ 07-report-server/src/reports/statistics.report.ts
@@ -1,5 +1,6 @@
 import type{ TDocumentDefinitions } from "pdfmake/interfaces";
 import * as Utils from 'src/helpers/chart-utils';
+import { getDonutChart } from './charts/donut.chart';
 
 
 interface TopCountry{
@@ -13,51 +14,19 @@ interface ReportOptions {
   topCountries: TopCountry[];
 }
 
-const generateTopCountriesDonut =
-    async  (topCountries: TopCountry[]) :Promise<string> =>{
-  const data = {
-    labels: topCountries.map((country)  => country.country),
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: topCountries.map((country) => country.customers),
-       // backgroundColor: Object.values(Utils.CHART_COLORS),
-      }
-    ]
-  };
 
-      const config = {
-        type: 'doughnut',
-        data: data,
-        options: {
-          responsive: true,
-          legend: {
-            position: 'left',
-          },
-          title: {
-            text: 'Chart.js Doughnut Chart',
-            display: true,
-          },
-          plugins: {
-            datalabels: {
-              color: 'white',
-              font:{
-                weight: 'bold',
-                size: 14,
-              }
-            },
-          }
-        },
-      };
-
-  return Utils.chartJSToImage(config)
-}
 
 export const getStatisticReport =  async (
     options: ReportOptions):
     Promise<TDocumentDefinitions> => {
 
-  const dountChart = await generateTopCountriesDonut(options.topCountries);
+  const dountChart = await getDonutChart({
+    entries: options.topCountries.map((c) => ({
+      label: c.country,
+        value: c.customers
+    })),
+    posicion: 'left'
+  });
 
   const docDefinition : TDocumentDefinitions = {
     content: [
